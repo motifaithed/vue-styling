e1e59169c8463e460773885c200959478f320413
Ronel Andaya <ronel.andaya@gmail.com>
2022-08-19 06:52:41 +0800

    022 Adding CSS Classes Dynamically

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            boxSelectedA: false,
-            boxSelectedB: false,
-            boxSelectedC: false,
-        }
+  data() {
+    return {
+      boxSelectedA: false,
+      boxSelectedB: false,
+      boxSelectedC: false,
+    };
+  },
+  methods: {
+    selectBox(box) {
+      console.log(box);
+      if (box === "A") {
+        this.boxSelectedA = !this.boxSelectedA;
+      } else if (box === "B") {
+        this.boxSelectedB = !this.boxSelectedB;
+      } else {
+        this.boxSelectedC = !this.boxSelectedC;
+      }
     },
-    methods:{
-        selectBox(box){
-            console.log(box);
-            if(box === 'A'){
-                this.boxSelectedA =  true;
-                this.boxSelectedB =  false;
-                this.boxSelectedC =  false;
-            }else if(box === 'B'){
-                this.boxSelectedA =  false;
-                this.boxSelectedB =  true;
-                this.boxSelectedC =  false;
-            }else{
-                this.boxSelectedA =  false;
-                this.boxSelectedB =  false;
-                this.boxSelectedC =  true;
-            }
-        }
-    }
+  },
 });
 
-app.mount('#styling');
+app.mount("#styling");
